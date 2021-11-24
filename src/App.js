7ce5b70f7fe7a2a75a2a6ab47666155a1f590e4d
daniel <daniel.zapata.682@gmail.com>
2021-11-23 22:08:06 -0500

    hotfix

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
       setTimer(rest);
       setDescanso(true);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [active, seconds]);
 
   useEffect(() => {
